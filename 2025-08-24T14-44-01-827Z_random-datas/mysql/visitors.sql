951a48f7d66e6bbec005cb2e54050165a4eb5417
Rogério Soares <rgrsoares@yahoo.com.br>
2025-08-24 11:47:32 -0300

    Add separation of databases types

---
+++ 2025-08-24T14-44-01-827Z_random-datas/mysql/visitors.sql
@@ -0,0 +1,38 @@
+-- Dados de visitors
+CREATE TABLE IF NOT EXISTS visitors (
+  id TEXT,
+  articles_id TEXT,
+  ip TEXT,
+  acessed_in TEXT
+);
+
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('1', '15', '24d7:ed95:ee44:df16:a211:816f:a66d:a2b8', 'Thu Jul 03 2025 20:11:36 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('2', '57', '0f68:d8be:2a33:d6dd:174f:7c9d:adcb:5eba', 'Fri Jun 13 2025 16:07:51 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('3', '94', '12.25.185.108', 'Sun Jul 20 2025 06:53:07 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('4', '86', '195.96.11.188', 'Fri Aug 15 2025 15:05:55 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('5', '85', 'd697:44f6:f4db:2de5:95d4:5e1e:cf74:bbda', 'Sat Jun 14 2025 05:22:58 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('6', '69', '8cc2:74da:1712:bb63:cfd0:5a3c:52b8:d627', 'Sat Aug 23 2025 02:10:07 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('7', '1', '371e:df2f:a4bf:70a0:ed7c:0d2b:0cea:8663', 'Mon Jul 28 2025 12:19:56 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('8', '23', 'e32e:34da:a9a5:1c2e:a2fc:affa:ef0a:a0dc', 'Tue Jul 01 2025 13:54:55 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('9', '83', '135.192.17.14', 'Tue Jul 15 2025 14:41:57 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('10', '82', '186.41.180.2', 'Sun Jun 22 2025 09:55:25 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('11', '97', '105.213.239.199', 'Sat Jul 05 2025 06:36:10 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('12', '37', '6cac:efd7:b490:dfed:be17:d055:fdd7:3e94', 'Thu Jun 05 2025 23:49:01 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('13', '91', '69.191.45.121', 'Sun May 18 2025 10:39:13 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('14', '49', '170.183.144.97', 'Sat Aug 02 2025 02:42:21 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('15', '35', '415a:a388:6de3:eba5:02da:09e3:c0c9:709a', 'Sun May 18 2025 19:47:54 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('16', '34', '4ecf:f4f5:e6b6:c939:8e59:d3fe:cadc:a03b', 'Fri Jun 06 2025 02:56:59 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('17', '46', 'abda:f9ab:9ccd:3b2e:8bd1:d5f8:a73d:b529', 'Sat Aug 23 2025 19:11:51 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('18', '95', '0ce3:ceb1:682b:5a2f:c1bb:e881:34e2:dffd', 'Mon Jul 28 2025 04:19:46 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('19', '72', '68a5:3a79:f495:b10a:b8fd:9ada:28fd:d9de', 'Sun Jul 20 2025 02:16:22 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('20', '41', '121.55.161.75', 'Thu Aug 14 2025 14:46:50 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('21', '7', '249.149.93.214', 'Sat Jun 21 2025 14:23:59 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('22', '30', 'a172:85db:6c88:8adb:96ba:53da:022e:becf', 'Thu May 29 2025 19:51:25 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('23', '92', 'c8e8:fcff:cd8b:aa1c:8d6d:0f4f:be9f:52dc', 'Sun Aug 17 2025 17:15:23 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('24', '27', 'abdf:cf78:d81d:66a4:e006:6bfe:cdb5:1bc4', 'Mon Aug 11 2025 23:50:22 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('25', '25', '1b43:7354:c3fc:ee86:48ab:8df9:2731:9e4b', 'Wed Aug 06 2025 07:35:35 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('26', '58', '89cf:7a9c:4ff0:bdff:a60a:f04d:cddb:5d2d', 'Fri Jun 13 2025 18:47:40 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('27', '57', '2.55.203.109', 'Thu Aug 14 2025 16:30:03 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('28', '43', '16.125.26.187', 'Mon Jul 07 2025 20:36:48 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('29', '67', '157.93.221.185', 'Thu Jun 12 2025 02:52:45 GMT-0300 (Brasilia Standard Time)');
+INSERT INTO visitors (id, articles_id, ip, acessed_in) VALUES ('30', '82', 'badb:e7c9:3f3e:4156:ac1f:1c38:f1cc:7010', 'Wed Jul 16 2025 18:02:06 GMT-0300 (Brasilia Standard Time)');
--- generateFaker.js
+++ generateFaker.js
@@ -3,108 +3,155 @@ const path = require('path');
 const bcrypt = require('bcryptjs');
 const { faker } = require('@faker-js/faker');
 
-// Pastas de saída
-const timestamp = new Date().toISOString().replace(/[:.]/g,'-');
+// Pasta raiz de saída
+const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
 const outputDir = `${timestamp}_random-datas`;
 if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
+
+// Subpastas para cada banco
+const dbs = ['mysql', 'postgres', 'mongodb'];
+dbs.forEach(db => {
+  const dbPath = path.join(outputDir, db);
+  if (!fs.existsSync(dbPath)) fs.mkdirSync(dbPath);
+});
+
+// Funções auxiliares
+function saveJSON(db, filename, data) {
+  fs.writeFileSync(path.join(outputDir, db, filename + '.json'), JSON.stringify(data, null, 2));
+}
+
+function saveSQL(db, filename, table, data) {
+  let sql = `-- Dados de ${table}\nCREATE TABLE IF NOT EXISTS ${table} (\n`;
+  const keys = Object.keys(data[0]);
+
+  // Estrutura simples (tudo TEXT)
+  sql += keys.map(k => `  ${k} TEXT`).join(',\n') + `\n);\n\n`;
+
+  data.forEach(row => {
+    const values = keys.map(k => row[k] !== null ? `'${String(row[k]).replace(/'/g, "''")}'` : 'NULL').join(', ');
+    sql += `INSERT INTO ${table} (${keys.join(', ')}) VALUES (${values});\n`;
+  });
+
+  fs.writeFileSync(path.join(outputDir, db, filename + '.sql'), sql);
+}
 
 // --- Authors ---
 function generateAuthors(num = 50) {
   const authors = [];
-  for (let i=1; i<=num; i++) {
-    const password = faker.internet.password(12, false, /[A-Za-z0-9@#$%^&*()_+=-]/);
+  for (let i = 1; i <= num; i++) {
+    const password = faker.internet.password({ length: 12 });
     const hash_js = bcrypt.hashSync(password, 10);
-    const hash_php = bcrypt.hashSync(password, 10); // simula salt PHP
+    const hash_php = bcrypt.hashSync(password, 10);
     const nickname = faker.internet.username().toLowerCase();
     const name = faker.person.firstName();
     const surname = faker.person.lastName();
-    const email = faker.internet.email(name, surname).toLowerCase();
-    const status = Math.random() < 0.5 ? "0" : "1";
+    const email = faker.internet.email({ firstName: name, lastName: surname }).toLowerCase();
+    const status = faker.datatype.boolean() ? "1" : "0";
 
-    authors.push({ id:i, nickname, name, surname, email, password, hash_js, hash_php, status });
+    authors.push({ id: i, nickname, name, surname, email, password, hash_js, hash_php, status });
   }
-  fs.writeFileSync(path.join(outputDir,'authors.json'), JSON.stringify(authors,null,2));
+
+  saveJSON('mongodb', 'authors', authors);
+  saveSQL('mysql', 'authors', 'authors', authors);
+  saveSQL('postgres', 'authors', 'authors', authors);
+
   return authors.length;
 }
 
 // --- Categories ---
 function generateCategories(num = 30) {
   const categories = [];
-  for (let i=1; i<=num; i++) {
+  for (let i = 1; i <= num; i++) {
     const name = faker.commerce.department();
-    const slug = name.toLowerCase().replace(/\s+/g,'-');
+    const slug = name.toLowerCase().replace(/\s+/g, '-');
     let description = '';
-    for(let j=0;j<4;j++){
-      description += `<p>${faker.lorem.paragraph(50)}</p>\n\n`;
+    for (let j = 0; j < 4; j++) {
+      description += `<p>${faker.lorem.paragraph({ min: 30, max: 50 })}</p>\n\n`;
     }
-    const status = Math.random()<0.5?"0":"1";
-    categories.push({id:i,name,slug,description,status});
+    const status = faker.datatype.boolean() ? "1" : "0";
+    categories.push({ id: i, name, slug, description, status });
   }
-  fs.writeFileSync(path.join(outputDir,'categories.json'), JSON.stringify(categories,null,2));
+
+  saveJSON('mongodb', 'categories', categories);
+  saveSQL('mysql', 'categories', 'categories', categories);
+  saveSQL('postgres', 'categories', 'categories', categories);
+
   return categories.length;
 }
 
 // --- Articles ---
-function generateArticles(num = 100, maxAuthor=50, maxCategory=30) {
+function generateArticles(num = 100, maxAuthor = 50, maxCategory = 30) {
   const articles = [];
-  for (let i=1;i<=num;i++){
+  for (let i = 1; i <= num; i++) {
     const title = faker.lorem.sentence();
     const image = `https://picsum.photos/seed/${i}/500`;
     let content = '';
-    const totalWords = faker.datatype.number({min:1000,max:3000});
+    const totalWords = faker.number.int({ min: 1000, max: 3000 });
     let wordCount = 0;
-    while(wordCount < totalWords){
+    while (wordCount < totalWords) {
       const choice = Math.random();
-      if(choice < 0.1){
+      if (choice < 0.1) {
         content += `<h2>${faker.lorem.sentence()}</h2>\n`;
-      }else if(choice < 0.15){
-        content += `<img src="https://picsum.photos/seed/${faker.datatype.uuid()}/500" style="display:block;margin:0 auto;max-width:500px;" />\n`;
-      }else{
+      } else if (choice < 0.15) {
+        content += `<img src="https://picsum.photos/seed/${faker.string.uuid()}/500" style="display:block;margin:0 auto;max-width:500px;" />\n`;
+      } else {
         const para = faker.lorem.paragraph();
         content += `<p>${para}</p>\n`;
         wordCount += para.split(' ').length;
       }
     }
-    const status = Math.random()<0.5?"0":"1";
-    const created = faker.date.past(3);
-    const updated = faker.date.between(created,new Date());
-    const authors_id = faker.datatype.number({min:1,max:maxAuthor});
-    const categories_id = faker.datatype.number({min:1,max:maxCategory});
+    const status = faker.datatype.boolean() ? "1" : "0";
+    const created = faker.date.past({ years: 3 });
+    const updated = faker.date.between({ from: created, to: new Date() });
+    const authors_id = faker.number.int({ min: 1, max: maxAuthor });
+    const categories_id = faker.number.int({ min: 1, max: maxCategory });
 
-    articles.push({id:i,title,image,content,status,created,updated,authors_id,categories_id});
+    articles.push({ id: i, title, image, content, status, created, updated, authors_id, categories_id });
   }
-  fs.writeFileSync(path.join(outputDir,'articles.json'), JSON.stringify(articles,null,2));
+
+  saveJSON('mongodb', 'articles', articles);
+  saveSQL('mysql', 'articles', 'articles', articles);
+  saveSQL('postgres', 'articles', 'articles', articles);
+
   return articles.length;
 }
 
 // --- Newsletters ---
-function generateNewsletters(num=30){
-  const newsletters=[];
-  for(let i=1;i<=num;i++){
+function generateNewsletters(num = 30) {
+  const newsletters = [];
+  for (let i = 1; i <= num; i++) {
     newsletters.push({
-      id:i,
-      name: faker.name.findName(),
+      id: i,
+      name: faker.person.fullName(),
       email: faker.internet.email(),
       ip: faker.internet.ip(),
-      registered_in: faker.date.recent(100)
+      registered_in: faker.date.recent({ days: 100 })
     });
   }
-  fs.writeFileSync(path.join(outputDir,'newsletters.json'), JSON.stringify(newsletters,null,2));
+
+  saveJSON('mongodb', 'newsletters', newsletters);
+  saveSQL('mysql', 'newsletters', 'newsletters', newsletters);
+  saveSQL('postgres', 'newsletters', 'newsletters', newsletters);
+
   return newsletters.length;
 }
 
 // --- Visitors ---
-function generateVisitors(num=30,maxArticles=100){
-  const visitors=[];
-  for(let i=1;i<=num;i++){
+function generateVisitors(num = 30, maxArticles = 100) {
+  const visitors = [];
+  for (let i = 1; i <= num; i++) {
     visitors.push({
-      id:i,
-      articles_id: faker.datatype.number({min:1,max:maxArticles}),
+      id: i,
+      articles_id: faker.number.int({ min: 1, max: maxArticles }),
       ip: faker.internet.ip(),
-      acessed_in: faker.date.recent(100)
+      acessed_in: faker.date.recent({ days: 100 })
     });
   }
-  fs.writeFileSync(path.join(outputDir,'visitors.json'), JSON.stringify(visitors,null,2));
+
+  saveJSON('mongodb', 'visitors', visitors);
+  saveSQL('mysql', 'visitors', 'visitors', visitors);
+  saveSQL('postgres', 'visitors', 'visitors', visitors);
+
   return visitors.length;
 }
 
@@ -112,7 +159,14 @@ function generateVisitors(num=30,maxArticles=100){
 const authorsCount = generateAuthors();
 const categoriesCount = generateCategories();
 const articlesCount = generateArticles(100, authorsCount, categoriesCount);
-generateNewsletters();
-generateVisitors(30, articlesCount);
+const newslettersCount = generateNewsletters();
+const visitorsCount = generateVisitors(30, articlesCount);
 
-console.log(`Todos os dados gerados na pasta ${outputDir}`);
+console.log(`✅ Dados gerados em: ${outputDir}`);
+console.log(`📊 Resumo: 
+  - ${authorsCount} authors
+  - ${categoriesCount} categories
+  - ${articlesCount} articles
+  - ${newslettersCount} newsletters
+  - ${visitorsCount} visitors
+`);
